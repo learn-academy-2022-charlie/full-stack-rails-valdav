cda0d7a7cdc311d8bcd3040e4bab2806a8a03db5
vjtovar <vjtovar25@gmail.com>
2022-06-16 10:20:06 -0700

    on stretch

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do 
   get 'blogs' => 'blog#index', as:'blogs'
   get 'blogs/new' => 'blog#new', as: 'new_blog'
-  get '/blogs/:id' => 'blog#show', as:'blog'
   post 'blogs' => 'blog#create'
+  get '/blogs/:id' => 'blog#show', as:'blog'
+ 
   root 'blog#index'
 end
+
